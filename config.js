d6d2d1db76f9ccf82c2734c7e65c9e51734067dc
Ryohei Kida <r.kida.tky@gmail.com>
2026-05-08 14:05:01 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// GAS連携設定
+// Google Apps ScriptのウェブアプリURL（/execで終わるURL）を貼り付けてください。
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxL-9sbsz9mHtpD_jmV7oAFop0Ze_N86IWldQFawEjP68eh8r0Zvr3HhQnGarW8PZft/exec";
+// GAS側のEVENT_CODEと同じ文字列にしてください。
+const EVENT_CODE = "SUPPORTQUEST2026";
