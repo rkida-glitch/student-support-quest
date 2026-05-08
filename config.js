dc3d5c9b755e5671b5ba0d937326881f05990452
Ryohei Kida <r.kida.tky@gmail.com>
2026-05-08 14:26:59 +0900

    Update GAS_WEB_APP_URL in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // GAS連携設定
 // Google Apps ScriptのウェブアプリURL（/execで終わるURL）を貼り付けてください。
-const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxL-9sbsz9mHtpD_jmV7oAFop0Ze_N86IWldQFawEjP68eh8r0Zvr3HhQnGarW8PZft/exec";
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwKu3xGwRuK8p9b0W2UkH8PfzALQBJXFbRA08IQqsVay-NxYoH4tC-LjTnMJ0XC-rY1/exec";
 // GAS側のEVENT_CODEと同じ文字列にしてください。
 const EVENT_CODE = "SUPPORTQUEST2026";
