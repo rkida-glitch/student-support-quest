ce409376677a21efe58ab47aec265e8502814204
Ryohei Kida <r.kida.tky@gmail.com>
2026-05-08 14:58:22 +0900

    Change GAS web app URL in config.js
    Updated the Google Apps Script web app URL in the configuration.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // GAS連携設定
 // Google Apps ScriptのウェブアプリURL（/execで終わるURL）を貼り付けてください。
-const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxo6R4FxiHOX6zEqrwYbF1E_DzI2cwNQPYwa7Qlslege0762UpPaK8M0N-y8zz7hG-M/exec";
+const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzZRWmvvqtctbhR5Hv62j212p5I-xjK6ufYugK0ascf3WN2d9XUw3_5t8D6DI50ib_d/exec";
 // GAS側のEVENT_CODEと同じ文字列にしてください。
 const EVENT_CODE = "SUPPORTQUEST2026";
